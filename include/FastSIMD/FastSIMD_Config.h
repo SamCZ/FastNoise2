fca178ceaaa2c4e896055fa5febff6c3c021a97c
SamCZ <rainb0wdash@seznam.cz>
2021-10-03 21:30:04 +0200

    Disabled AVX on MINGW Windows

--- include/FastSIMD/FastSIMD_Config.h
+++ include/FastSIMD/FastSIMD_Config.h
@@ -12,9 +12,15 @@
 #define FASTSIMD_ARM false
 #endif
 
+#if defined(__MINGW32__) && defined(__WIN32)
+#define FASTSIMD_AVX_SUPPORTED false
+#else
+#define FASTSIMD_AVX_SUPPORTED true
+#endif
+
 #define FASTSIMD_64BIT (INTPTR_MAX == INT64_MAX)
 
-#define FASTSIMD_COMPILE_SCALAR (!(FASTSIMD_x86 && FASTSIMD_64BIT)) // Don't compile for x86 64bit since CPU is guaranteed SSE2 support 
+#define FASTSIMD_COMPILE_SCALAR (!(FASTSIMD_x86 && FASTSIMD_64BIT)) // Don't compile for x86 64bit since CPU is guaranteed SSE2 support
 
 #define FASTSIMD_COMPILE_SSE    (FASTSIMD_x86 & false) // Not supported
 #define FASTSIMD_COMPILE_SSE2   (FASTSIMD_x86 & true )
@@ -22,9 +28,9 @@
 #define FASTSIMD_COMPILE_SSSE3  (FASTSIMD_x86 & true )
 #define FASTSIMD_COMPILE_SSE41  (FASTSIMD_x86 & true )
 #define FASTSIMD_COMPILE_SSE42  (FASTSIMD_x86 & true )
-#define FASTSIMD_COMPILE_AVX    (FASTSIMD_x86 & false) // Not supported
-#define FASTSIMD_COMPILE_AVX2   (FASTSIMD_x86 & true )
-#define FASTSIMD_COMPILE_AVX512 (FASTSIMD_x86 & true )
+#define FASTSIMD_COMPILE_AVX    (FASTSIMD_x86 & FASTSIMD_AVX_SUPPORTED & false) // Not supported
+#define FASTSIMD_COMPILE_AVX2   (FASTSIMD_x86 & FASTSIMD_AVX_SUPPORTED & true )
+#define FASTSIMD_COMPILE_AVX512 (FASTSIMD_x86 & FASTSIMD_AVX_SUPPORTED & true )
 
 #define FASTSIMD_COMPILE_NEON   (FASTSIMD_ARM & true )
 
